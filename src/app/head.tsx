6fa8cdae7987c01b92e4c77cb3f09b5b979b4f10
Drew Wyatt <drew.j.wyatt@gmail.com>
2023-01-08 12:48:08 -0500

    remove cruft

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,14 +1,8 @@
-import { DefaultTags } from '#/ui/DefaultTags'
-
 export default function Head() {
   return (
     <>
-      <DefaultTags />
-      <title>Next.js App Directory Playground</title>
-      <meta
-        name="description"
-        content="A playground to explore new Next.js 13 app directory features such as nested layouts, instant loading states, streaming, and component level data fetching."
-      />
+      <title>Logic Puzzles</title>
+      <meta name="description" content="Use JS to complete logic puzzles" />
     </>
   )
 }
